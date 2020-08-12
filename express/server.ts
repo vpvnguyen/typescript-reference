d56bffef39c8c934da2f4ffe9b855ba656358c16
vpvnguyen <vpvnguyen@gmail.com>
2020-08-12 12:50:34 -0700

    defined express request and response types

--- express/server.ts
+++ express/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { Request, Response } from "express";
 const app = express();
 
-const PORT = 5000;
+const PORT: number = 5000;
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Express TS Server");
 });
 
